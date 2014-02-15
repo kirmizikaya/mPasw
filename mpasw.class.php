bf9b4ef3b146903dcab6e68a4f620a19f35a74ff
mtutuncu <mucahit@Maxtor-MacBook-Air.local>
2014-02-15 12:13:56 +0200

    -
    -

---
+++ mpasw.class.php
@@ -0,0 +1,36 @@
+<?php
+	
+	/*
+		05.02.2014 @ 03:51
+		@mucahittutuncu
+	*/
+
+	class mpasw
+	{
+		#The value to be encrypted.
+			public static $value;
+
+		#Encrypted value.
+			public static $encryptedValue;
+
+		#encrypting function.
+			public static function encrypting($var)
+			{
+
+				if(!empty($var))
+				{
+
+					self::$value 			= $var;
+					$oneStep				= md5(self::$value);
+					$twoStep				= sha1($oneStep);
+					$threeStep				= substr($twoStep, 0,32);
+					self::$encryptedValue	= $threeStep;
+					return self::$encryptedValue;
+					
+				}
+
+			}
+
+	}
+	echo mpasw::encrypting('yeah!');
+?>
